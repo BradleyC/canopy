007d7ba622624c62715c79c9f970f05505e319ae
Bradley C <bradleyctclarke@gmail.com>
2018-12-08 20:59:00 +0800

    adding logic for scoring and ranking

--- contracts/Canopy.sol
+++ contracts/Canopy.sol
@@ -9,6 +9,7 @@ contract Canopy {
         string url;
         uint256 timePosted;
         uint256 stake;
+        uint256 voteTokens;
         uint256 score;
         uint256 valuePositive;
         uint256 valueNegative;
@@ -26,25 +27,30 @@ contract Canopy {
 
     /*** EVENTS ***/
     event NewPost(
-        uint256 indexed postId, 
-        address posterAddress, 
-        string title, 
-        string url, 
+        uint256 indexed postId,
+        address posterAddress,
+        string title,
+        string url,
         uint256 indexed timePosted, 
-        uint256 stake, 
-        uint256 score, 
+        uint256 stake,
+        uint256 score,
         bool active
     );
     // TODO: event NewVote(uint256 indexed postId, )
+    event ContractUpgrade(address indexed newContract);
 
     /*** STORAGE ***/
     Post[] posts;
     address[] private participants; // used for rolling jackpot
     uint256[40] public top40PostIds;
+    // below: uint256 to make sure we can fully support an enormous pool. 
+    uint256 public poolValue = address(this).balance;
 
     mapping (uint256 => address) public postIdToOwner;
     mapping (uint256 => uint256) public postIdToTime;
     mapping (uint256 => uint256) public postIdToScore;
+    mapping (uint256 => uint256) public postIdToVoteTokens;    
+    mapping (uint256 => address[]) public postIdToVoters;
     mapping (address => uint256) public userPostCount;
     mapping (address => uint256) public userToTotalScore;
     mapping (address => uint256) public userToVoterTally;
@@ -60,23 +66,24 @@ contract Canopy {
     // @dev function to create a new post
     function createPost(string title, string url, uint256 stake)
         // TODO: make sure "stake" transfer is executed properly  
-        external 
+        external
         returns (
             uint256,
             uint256
         ){
         // below: intentionally ignoring warning; time is non-critical
-        uint256 _timePosted = now;
-        address _poster = msg.sender;
+        uint256 timePosted = now;
+        address poster = msg.sender;
         // below: start score = (1 up - 0 down) * (1 voter / 1 second since post)
-        _score = 1;
-        Post memory _post = Post({
-            posterAddress: _poster,
+        uint256 score = 1;
+        Post memory post = Post({
+            posterAddress: poster,
             title: title,
             url: url,
-            timePosted: _timePosted,
+            timePosted: timePosted,
             stake: stake,
-            score: _score,
+            voteTokens: 0,
+            score: score,
             // regardless of stake value, self votes always count as one vote
             valuePositive: 1,
             valueNegative: 0,
@@ -84,22 +91,23 @@ contract Canopy {
             active: true
         });
 
-        uint256 newPostId = posts.push(_post) - 1;
-        postIdToOwner[newPostId] = _poster;
-        postIdToTime[newPostId] = _timePosted;
+        uint256 newPostId = posts.push(post) - 1;
+        postIdToOwner[newPostId] = poster;
+        postIdToTime[newPostId] = timePosted;
+        postIdToVoteTokens[newPostId] = 0;
         // below: new post is initiated with score of 1
         postIdToScore[newPostId]++;
-        userToTotalScore[_poster]++;
-        userPostCount[_poster]++;
-        userToVoterTally[_poster]++;
+        userToTotalScore[poster]++;
+        userPostCount[poster]++;
+        userToVoterTally[poster]++;
         emit NewPost(
             newPostId, 
-            _poster, 
+            poster, 
             title, 
             url, 
-            _timePosted, 
+            timePosted, 
             stake, 
-            _score, 
+            score, 
             true
         );
         return (newPostId, score);
@@ -136,14 +144,16 @@ contract Canopy {
     }
 
     // @dev fetch the list of ranked posts
-    function getRankedPosts() internal pure {
-        getPost();
+    function getRankedPosts() internal view returns (uint256[]) {
+        return top40PostIds;
     }
 
     // @dev pass in vote params. return the vote weight instantly, re-rank posts
     function vote() external returns (uint256) {
         // voting logics
         getScoreById();
+        scorePost();
+        // if score is greater than lowest in top 40
         rankPosts();
         // update mappings
         return score;
@@ -151,15 +161,62 @@ contract Canopy {
 
     // @dev process to rank posts
     // evaluate score and if applicable, add to post top 40
-    function rankPosts() internal {}
+    function rankPosts(uint256 _postId, uint256 _score) internal {
+        // we're only entering this loop if we know that we have a score that displaces a high ranking score
+        // update all scores
+        for (uint i = 0; i < 40; i++) {
+            uint currentId = top40PostIds[i];
+            Post memory c = posts[currentId];
+            if (( now - c.timePosted) > 2592000) {
+                c.active = false;
+                c.score = 0;
+                cashOut(currentId);
+            }
+            // below is a lazy way to move inactive posts to the bottom of the top 40. 
+            // it may take a view votes to force them out entirely.
+            else if (c.active = false) {
+                c.score = 0;
+            }
+            else scorePost(currentId);
+        }
+        // SORT
 
-    // ranking
-    // iterate over vote totals in list starting with floor = 0
-    // create inner top 10 array biggest -> smallest
-    // if vote total > floor, loop through inner array.
-    // if vote total < current object in inner array, insert and pop last element
-    
-    function rankPosts() internal {}
+
+        // score floor
+        // for p in posts
+        // evaluate if score > floor
+        // force pay out stale posts
+        // merge sort?
+        // ranking
+        // iterate over vote totals in list starting with floor = 0
+        // create inner top 40 array biggest -> smallest
+        // if vote total > floor, loop through inner array.
+        // if vote total < current object in inner array, insert and pop last element
+    }
+
+    function scorePost(uint256 _postId) internal returns (uint256) {
+        Post memory p = posts[_postId];
+        uint256 uproot = sqrt(p.valuePositive);
+        uint256 downroot = sqrt(p.valueNegative);
+        uint256 totalVoters = p.voterTally;
+        uint256 rootTimeSincePost = sqrt(now - p.timePosted); 
+        uint256 _score = (uproot / downroot)(totalVoters / rootTimeSincePost);
+        posts[_postId].score = _score;
+        return _score;
+    }
+
+    // below method is borrowed from https://github.com/ethereum/dapp-bin/pull/50/files
+    function sqrt(uint256 x) internal returns (uint256 y) {
+        if (x == 0) return 0;
+        else if (x <= 3) return 1;
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y)
+        {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
 
     // @dev read from mapping
     function getPostsByTime() external {}
@@ -168,7 +225,9 @@ contract Canopy {
     function getScoreById() internal {}
 
     // @dev user can choose when to cash out, make sure to check address
-    function cashOut() external payable onlyPoster {}
+    function cashOut(_postId) external payable onlyPoster {
+        return;
+    }
 
     // @dev BONUS - get post with highest tally of votes
     // @dev BONUS - get user's total score
